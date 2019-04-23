a997cafc09084ed6fe0c510159eef50743848782
Richard Lea <chigix@zoho.com>
2019-04-23 19:34:10 +0900

    fix(test): router dependency in navigation component spec
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- src/app/modules/navigation/navigation.component.spec.ts
+++ src/app/modules/navigation/navigation.component.spec.ts
@@ -1,6 +1,7 @@
 import { LayoutModule } from '@angular/cdk/layout';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 import {
   MatButtonModule,
   MatIconModule,
@@ -19,6 +20,7 @@ describe('NavigationComponent', () => {
     TestBed.configureTestingModule({
       declarations: [NavigationComponent],
       imports: [
+        RouterTestingModule,
         NoopAnimationsModule,
         LayoutModule,
         MatButtonModule,
