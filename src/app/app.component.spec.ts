4ca783ce4f692985f04921c0de6f894618e67b20
Richard Lea <chigix@zoho.com>
2019-04-23 19:25:55 +0900

    fix(test): app.component.spec.ts
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,39 @@
 import { TestBed, async } from '@angular/core/testing';
+import { APP_BASE_HREF } from '@angular/common';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavigationComponent } from './modules/navigation/navigation.component';
+import { LinkedDataGraphicModule } from 'linked-data-graphic';
+import {
+  MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule,
+  MatListModule,
+} from '@angular/material';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'quickstart', component: HomeComponent },
+];
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        // RouterTestingModule
+        RouterModule.forRoot(routes), NoopAnimationsModule,
+        MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule,
+        MatListModule,
+        LinkedDataGraphicModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        NavigationComponent,
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
       ],
     }).compileComponents();
   }));
@@ -22,14 +46,14 @@ describe('AppComponent', () => {
 
   it(`should have as title 'linked-data-graphic'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.debugElement.componentInstance as AppComponent;
     expect(app.title).toEqual('linked-data-graphic');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to linked-data-graphic!');
+    const compiled = fixture.debugElement.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Linked Data Graphic Component');
   });
 });
