130a6d8af663a82c369015660de88404a93abd84
Richard Lea <chigix@zoho.com>
2020-10-28 14:53:15 +0900

    feat(canvas): custom-icon-registry manages icons in run

---
+++ projects/linked-data-graphic/src/lib/svg-icon-provider/custom-icon-registry.service.spec.ts
@@ -0,0 +1,61 @@
+import { HttpClient } from '@angular/common/http';
+import { ErrorHandler } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { CustomIconRegistry, SvgIconInfo } from './custom-icon-registry.service';
+
+function createSvg(svgSrc: string): SVGSVGElement {
+  const div = document.createElement('div');
+  div.innerHTML = svgSrc;
+  return div.querySelector('svg');
+}
+
+describe('CustomIconRegistryService', () => {
+
+  const fakeHttpClient: HttpClient = {} as any;
+  const fakeDomSanitizer: DomSanitizer = {} as any;
+  const fakeDocument: Document = {} as any;
+  const fakeErrorHandler: ErrorHandler = { handleError: err => console.error(err) };
+
+  it('should get the SVG element for a preloaded icon from the cache', (done) => {
+    const svgSrc1 = '<svg xmlns="http://www.w3.org/2000/svg"><path d="h100" /></svg>';
+    const svgSrc2 = '<svg xmlns="http://www.w3.org/2000/svg"><path d="h200" /></svg>';
+    const svgSrc3 = '<svg xmlns="http://www.w3.org/2000/svg"><path d="h300" /></svg>';
+    const svgIcons: SvgIconInfo[] = [
+      { name: 'test_icon', svgSource: svgSrc1 },
+      { namespace: 'jikai', name: 'test_icon', svgSource: svgSrc2 },
+      { namespace: 'bankai', name: 'test_icon', svgSource: svgSrc3 },
+    ];
+
+    const registry = new CustomIconRegistry(
+      fakeHttpClient, fakeDomSanitizer, fakeDocument, fakeErrorHandler, svgIcons
+    );
+    registry.getNamedSvgIcon('test_icon', 'jikai').toPromise().then(
+      svgElement => {
+        expect(svgElement).toEqual(createSvg(svgSrc2));
+        done();
+      }
+    );
+  });
+
+  it('should call through to the MdIconRegistry if the icon name is not in the preloaded cache', () => {
+    const svgSrc = '<svg xmlns="http://www.w3.org/2000/svg" focusable="false" ' +
+      'viewBox="0 0 24 24"><path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/></svg>';
+    const svgIcons: SvgIconInfo[] = [
+      { name: 'test_icon', svgSource: svgSrc }
+    ];
+    spyOn(MatIconRegistry.prototype, 'getNamedSvgIcon');
+
+    const registry = new CustomIconRegistry(
+      fakeHttpClient, fakeDomSanitizer, fakeDocument, fakeErrorHandler, svgIcons,
+    );
+
+    registry.getNamedSvgIcon('other_icon');
+    expect(MatIconRegistry.prototype.getNamedSvgIcon).toHaveBeenCalledWith('other_icon', undefined);
+
+    registry.getNamedSvgIcon('other_icon', 'foo');
+    expect(MatIconRegistry.prototype.getNamedSvgIcon).toHaveBeenCalledWith('other_icon', 'foo');
+  });
+
+});
---
+++ projects/linked-data-graphic/src/lib/svg-icon-provider/custom-icon-registry.service.ts
@@ -0,0 +1,81 @@
+import { ErrorHandler, Inject, Injectable, InjectionToken, Optional } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Observable, of } from 'rxjs';
+
+export const SVG_ICONS = new InjectionToken<Array<SvgIconInfo>>('SvgIcons');
+
+export interface SvgIconInfo {
+  namespace?: string;
+  name: string;
+  svgSource: string;
+}
+
+interface SvgIconMap {
+  [namespace: string]: {
+    [iconName: string]: SVGElement;
+  };
+}
+
+const DEFAULT_NS = '$$default';
+
+/**
+ * https://github.com/angular/angular/blob/f8096d499324cf0961f092944bbaedd05364eea1/aio/src/app/shared/custom-icon-registry.ts
+ */
+@Injectable()
+export class CustomIconRegistry extends MatIconRegistry {
+
+  private cachedSvgElements: SvgIconMap = { [DEFAULT_NS]: {} };
+
+  constructor(
+    http: HttpClient,
+    sanitizer: DomSanitizer,
+    @Optional() @Inject(DOCUMENT) document: any,
+    errorHandler: ErrorHandler,
+    @Inject(SVG_ICONS) private svgIcons: SvgIconInfo[],
+  ) {
+    super(http, sanitizer, document, errorHandler);
+  }
+
+  /**
+   * getNamedSvgIcon
+   */
+  public getNamedSvgIcon(iconName: string, namespace?: string): Observable<SVGElement> {
+    const nsIconMap = this.cachedSvgElements[namespace || DEFAULT_NS];
+    let preloadedElement: SVGElement | undefined = nsIconMap && nsIconMap[iconName];
+    if (!preloadedElement) {
+      preloadedElement = this.loadSvgElement(iconName, namespace);
+    }
+    return preloadedElement
+      ? of(preloadedElement.cloneNode(true) as SVGElement)
+      : super.getNamedSvgIcon(iconName, namespace);
+
+  }
+
+  private loadSvgElement(iconName: string, namespace?: string): SVGElement | undefined {
+    const svgIcon = this.svgIcons.find(icon => {
+      return namespace
+        ? icon.name === iconName && icon.namespace === namespace
+        : icon.name === iconName;
+    });
+    if (!svgIcon) {
+      return;
+    }
+
+    const ns = svgIcon.namespace || DEFAULT_NS;
+    const nsIconMap = this.cachedSvgElements[ns] || (this.cachedSvgElements[ns] = {});
+
+    // Creating a new `<div>` per icon is necessary for the SVGs to work correctly in IE11.
+    const div = document.createElement('DIV');
+
+    // SECURITY: the source for the SVG icons is provided in code by trusted developers
+    div.innerHTML = svgIcon.svgSource;
+
+    const svgElement = div.querySelector('svg');
+    nsIconMap[svgIcon.name] = svgElement;
+
+    return svgElement;
+  }
+}
